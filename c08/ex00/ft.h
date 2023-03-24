55598d995f84af74a429f2110f05e9042edf4837
Aymen ASSAF <79019644+aymenssf@users.noreply.github.com>
2023-03-24 00:40:25 +0000

    2022-09-12

---
+++ c08/ex00/ft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aassaf <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/13 19:12:07 by aassaf            #+#    #+#             */
+/*   Updated: 2022/09/14 10:28:03 by aassaf           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
